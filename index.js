68ea9989d593fcffb800eadfde4a3626da309dc5
Carlos Rodriguez <carlos@s8f.org>
2012-09-12 19:19:34 -0700

    work in progress

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+var npm = require('npm')
+  , sha1 = require('./lib/sha1')
+  , readJson = require('read-package-json')
+  , path = require('path')
+
 function list (str) {
   return str.split(/ *, */).map(function (val) {
     return val.match(/^\d+$/) ? parseInt(val, 10) : val;
@@ -17,49 +22,74 @@ if (process.argv[2] === 'spawn') {
 
 var program = require('commander')
   .version(require('./package').version)
+  .usage('<command>')
   .option('-s, --service <name[@version]>', 'drone service to request, with optional semver (default: app)', 'app')
   .option('-r, --redis <port/host/host:port/list>', 'redis server(s) used by the service (can be comma-separated)', list)
 
+function ifErr (err) {
+  if (err) {
+    if (err.stack) {
+      console.error(err.stack);
+    }
+    else {
+      console.error(err);
+    }
+    process.exit(1);
+  }
+}
+
 program
   .command('spawn -- [cmd] [args...]')
-  .description('deploy the cwd to drones and spawn a command')
-  .option('-d, --cwd <dir>', 'directory which will be packed and sent to drones as the cwd. (default: cwd)', process.cwd())
+  .description('deploy a project to drones and spawn a command')
+  .option('-r, --root <dir>', 'project root which will be packed and sent to drones as the cwd. (default: cwd)', process.cwd())
   .option('--drones <count>', 'number of drones to spawn on. (default: all drones)')
   .option('--threads <count>', 'number of threads to spawn per drone. (default: drone\'s cpu count)')
   .action(function () {
-    console.log('spawn ' + spawnArgs);
+    var program = [].slice.call(arguments).pop();
+    readJson(path.join(program.root, 'package.json'), function (err, data) {
+      ifErr(err);
+
+      var name = data.name
+        , version = data.version
+
+      npm.load(function (err) {
+        ifErr(err);
+        npm.commands.cache(['add', program.root], function (err) {
+          ifErr(err);
+          var file = path.join(npm.cache, name, version, 'package.tgz');
+          sha1.get(file, function (err, sha1sum) {
+            ifErr(err);
+            console.log('cached project with sha1 sum: ' + sha1sum);
+          });
+        });
+      });
+    });
   })
 
 program
   .command('respawn [sha1]')
-  .description('respawn running processes, optionally on a particular sha1 hash')
+  .description('respawn running processes, optionally on a particular git sha1')
   .action(function (sha1) {
     console.log('respawn ' + sha1);
   })
 
 program
   .command('ps [sha1]')
-  .description('show running processes, optionally on a particular sha1 hash')
+  .description('show running processes, optionally on a particular git sha1')
   .action(function (sha1) {
     console.log('ps ' + sha1);
   })
 
 program
   .command('stop [sha1]')
-  .description('stop running processes, optionally on a particular sha1 hash')
+  .description('stop running processes, optionally on a particular git sha1')
   .action(function (sha1) {
     console.log('stop ' + sha1);
   })
 
 program
-  .command('sha1 [dir]')
-  .description('show the sha1 sum of the target dir')
-  .action(function (dir) {
-    console.log('sha1 ' + dir);
-  })
-
-program
   .command('*')
+  .description('output help')
   .action(function () {
     program.outputHelp();
   })
